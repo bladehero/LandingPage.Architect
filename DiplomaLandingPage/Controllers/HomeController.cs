53b6590685d45ecad7a5ca98ed44e76843c2246b
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2020-12-18 00:31:17 +0200

    Init

---
+++ DiplomaLandingPage/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace DiplomaLandingPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact(string name, string email, string message)
+        {
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DiplomaLandingPage/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DiplomaLandingPage.Startup))]
+
+namespace DiplomaLandingPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
